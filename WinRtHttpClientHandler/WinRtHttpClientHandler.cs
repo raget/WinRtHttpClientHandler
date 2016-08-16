d04f6514d7e814fe2d5bc09948ad4e579e9e7651
Radim Göth <radim.goeth@ysoft.com>
2016-08-16 13:30:35 +0200

    Refactoring

--- WinRtHttpClientHandler/WinRtHttpClientHandler.cs
+++ WinRtHttpClientHandler/WinRtHttpClientHandler.cs
@@ -21,42 +21,36 @@ namespace WindowsRuntime.HttpClientFilters
         private static readonly Version NoVersion = new Version(0, 0);
         private static readonly Version Version10 = new Version(1, 0);
         private static readonly Version Version11 = new Version(1, 1);
-        private readonly rt.HttpClient _client;
+        private readonly rt.HttpClient rtClient;
         private bool _disposed = false;
 
         public WinRtHttpClientHandler(IHttpFilter httpFilter)
         {
-            _client = new rt.HttpClient(httpFilter);
+            rtClient = new rt.HttpClient(httpFilter);
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             CheckDisposed();
-
-            var rtMessage = await ConvertHttpRequestMessaageToRt(request, cancellationToken).ConfigureAwait(false);
-
-            var resp = await _client.SendRequestAsync(rtMessage).AsTask(cancellationToken).ConfigureAwait(false);
-
-            var netResp = await ConvertRtResponseMessageToNet(resp, cancellationToken).ConfigureAwait(false);
-
-            return netResp;
+            var rtRequestMessage = await ConvertHttpRequestMessaageToRt(request, cancellationToken).ConfigureAwait(false);
+            var rtResponseMessage = await rtClient.SendRequestAsync(rtRequestMessage).AsTask(cancellationToken).ConfigureAwait(false);
+            return await ConvertRtResponseMessageToNet(rtResponseMessage, cancellationToken).ConfigureAwait(false);            
         }
 
         internal static async Task<rt.HttpRequestMessage> ConvertHttpRequestMessaageToRt(HttpRequestMessage message, CancellationToken token)
         {
-            var rt = new rt.HttpRequestMessage()
+            var requestmessage = new rt.HttpRequestMessage()
             {
                 Method = new rt.HttpMethod(message.Method.Method),
                 Content = await GetContentFromNet(message.Content).ConfigureAwait(false),
                 RequestUri = message.RequestUri,
             };
-
-            CopyHeaders(message.Headers, rt.Headers);
-
-            foreach (var prop in message.Properties)
-                rt.Properties.Add(prop);
-
-            return rt;
+            CopyHeaders(message.Headers, requestmessage.Headers);
+            foreach (var property in message.Properties)
+            {
+                requestmessage.Properties.Add(property);
+            }
+            return requestmessage;
         }
 
         internal static async Task<HttpRequestMessage> ConvertRtRequestMessageToNet(rt.HttpRequestMessage message, CancellationToken token)
@@ -67,13 +61,14 @@ namespace WindowsRuntime.HttpClientFilters
                 RequestUri = message.RequestUri,
                 Content = await GetNetContentFromRt(message.Content, token).ConfigureAwait(false),
             };
-
             foreach (var header in message.Headers)
+            {
                 req.Headers.TryAddWithoutValidation(header.Key, header.Value);
-
+            }
             foreach (var prop in message.Properties)
+            {
                 req.Properties.Add(prop);
-
+            }
             return req;
         }
 
@@ -82,35 +77,37 @@ namespace WindowsRuntime.HttpClientFilters
             var headers = from kvp in source
                 from val in kvp.Value
                 select new KeyValuePair<string, string>(kvp.Key, val);
+
             foreach (var header in headers)
+            {
                 destination.Add(header);
+            }
         }
 
         internal static async Task<rt.IHttpContent> GetContentFromNet(HttpContent content)
         {
             if (content == null)
+            {
                 return null;
-
+            }
             var stream = await content.ReadAsStreamAsync().ConfigureAwait(false);
             var c = new rt.HttpStreamContent(stream.AsInputStream());
-
             CopyHeaders(content.Headers, c.Headers);
-
             return c;
         }
 
         internal static async Task<HttpContent> GetNetContentFromRt(rt.IHttpContent content, CancellationToken token)
         {
             if (content == null)
+            {
                 return null;
-
+            }
             var str = await content.ReadAsInputStreamAsync().AsTask(token).ConfigureAwait(false);
-
             var c = new StreamContent(str.AsStreamForRead());
-
             foreach (var header in content.Headers)
+            {
                 c.Headers.TryAddWithoutValidation(header.Key, header.Value);
-
+            }
             return c;
         }
 
@@ -125,8 +122,9 @@ namespace WindowsRuntime.HttpClientFilters
             };
 
             foreach (var header in message.Headers)
+            {
                 resp.Headers.TryAddWithoutValidation(header.Key, header.Value);
-
+            }
             return resp;
         }
 
@@ -135,14 +133,11 @@ namespace WindowsRuntime.HttpClientFilters
             switch (version)
             {
                 case rt.HttpVersion.None:
-                    return NoVersion;
-                    break;
+                    return NoVersion;                    
                 case rt.HttpVersion.Http10:
-                    return Version10;
-                    break;
+                    return Version10;                    
                 case rt.HttpVersion.Http11:
-                    return Version11;
-                    break;
+                    return Version11;                    
                 default:
                     throw new ArgumentOutOfRangeException("version");
             }
@@ -158,11 +153,10 @@ namespace WindowsRuntime.HttpClientFilters
         {
             if (disposing && !_disposed)
             {
-                _client.Dispose();
+                rtClient.Dispose();
 
                 _disposed = true;
             }
-
             base.Dispose(disposing);
         }
     }
